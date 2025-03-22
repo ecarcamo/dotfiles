0036f17d4b796c43b96703b95d574d4cf78f6408
ecarcamo <estebancarcamou@gmail.com>
2025-03-21 18:17:30 -0600

    formatting html

---
+++ nvim/lua/plugins/format.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      html = { "prettier" },
+      css = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
+    },
+  },
+}
